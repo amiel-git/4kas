6b8bd95b81049d620166d48d0fdf143e39c72763
Amiel <afurongamielchrysdin@gmail.com>
2021-04-04 00:33:36 +0800

    Add project files.

---
+++ 4kas/4kas/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+
+[assembly: ExportFont("4kas.Assets.Fonts.Poppins-Regular.ttf", Alias = "Poppins")]
+[assembly: ExportFont("4kas.Assets.Fonts.Lobster-Regular.ttf", Alias = "Lobster")]
